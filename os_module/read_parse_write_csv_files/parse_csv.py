d8d1e1c84cb87d113c165f1618d764da9b229dbf
alinsultana605 <alinsultana605@gmail.com>
2026-07-28 23:06:37 +0300

    os_module

---
+++ os_module/read_parse_write_csv_files/parse_csv.py
@@ -0,0 +1,20 @@
+import csv
+
+# with open('new_names.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter='\t')
+
+#     for line in csv_reader:
+#         print(line)
+
+with open('name.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for line in csv_reader:
+         print(line['email'])
+    
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['firstname', 'last_name']
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+        csv_writer.writeheader()
+        for line in csv_reader:
+            del line['email']
+            csv_writer.writerow(line)
